11a80903fbed2f2c0a2bf768013dfdcfa12f1005
unknown <lotfimedo933@gmail.com>
2022-03-27 22:28:58 +0200

    first commit

---
+++ Lec/Assignments/Assignment1.c
@@ -0,0 +1,11 @@
+//Libraries including
+#include <stdio.h>
+void main(){
+    //Printing the pyramid
+    printf("     *     \n"
+           "    ***    \n"
+           "   *****   \n"
+           "  *******  \n"
+           " ********* \n"
+           "***********\n");
+}
---
+++ Lec/Assignments/Assignment3.c
@@ -0,0 +1,17 @@
+//Libraries including
+#include <stdio.h>
+void main (){
+    //Variables declaration
+    int a, b;
+    //User input
+	printf("Enter number a: ");
+    scanf(" %d", &a);
+    printf("\nEnter number b: ");
+    scanf(" %d", &b);
+    //Printing values
+    printf("\na + b = %d\n",a + b);
+    printf("\na - b = %d\n",a - b);
+    printf("\na & b = %d\n",a & b);
+    printf("\na | b = %d\n",a | b);
+    printf("\na ^ b = %d\n",a ^ b);
+}
